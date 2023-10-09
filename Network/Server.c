378e71a7ef590a3dad4a3438f6203a86cd9c9950
aberodz <abrahamrodz17@unitec.edu>
2023-10-08 21:21:50 -0600

    FIX: fixed segmentation error

--- Network/Server.c
+++ Network/Server.c
@@ -11,7 +11,8 @@ const char *http_response_headers = "HTTP/1.1 %d %s\r\n"
                                     "Content-Length: %zu\r\n"
                                     "\r\n";
 // Constructor of an http server
-struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog, void (*launch)(struct Server *server, RouteConfig *config))
+struct Server server_constructor(int domain, int service, int protocol, unsigned long interface,
+                                 int port, int backlog)
 {
     struct Server server;
     server.domain = domain;
@@ -44,7 +45,7 @@ struct Server server_constructor(int domain, int service, int protocol, unsigned
         perror("failed to listen...\n");
         exit(1);
     }
-    server.launch = launch;
+    // server.launch = *launch;
 
     return server;
 };
@@ -99,4 +100,80 @@ int response(int client_socket, int status_code, const char *json_data)
     }
 
     return 0;
+}
+
+void presentation(int port)
+{
+    printf(YEL "_________   ______  _______________________\n" RESET);
+    printf(YEL "__  ____/   ___  / / /__  __/__  __/__  __ \\\n" RESET);
+    printf(YEL "_  / _________  /_/ /__  /  __  /  __  /_/ /\n" RESET);
+    printf(YEL "/ /___/_____/  __  / _  /   _  /   _  ____/\n" RESET);
+    printf(YEL "\\____/      /_/ /_/  /_/    /_/    /_/\n" RESET);
+    printf(RED "Fast, portable and simple HTTP server!\n" RESET);
+    printf("Server running on port: " GRN "%d\n" RESET, port);
+}
+
+void start(struct Server *server, RouteConfig config)
+{
+    char buffer[30000];
+    char json[300];
+    snprintf(json, sizeof(json),
+             "{"
+             "   \"received_data\": \"hello\""
+             "}");
+    int address_length = sizeof(server->address);
+    int new_socket;
+    // Route *route = &config->routes[0];
+    for (int i = 0; i < config.num_routes; i++)
+    {
+        printf("Route: Method=%s, Path=%s\n", config.routes[i].method, config.routes[i].path);
+        // Call the associated request handler function
+        // config.routes[i].handler(123); // Passing a dummy context value
+    }
+    for (int i = 0; i < config.num_routes; i++)
+    {
+        Route route = config.routes[i];
+        if (&route != NULL)
+        {
+            printf("config is not NULL\n");
+            printf("%s %d\n", route.path, config.num_routes);
+            if (route.method != NULL)
+            {
+                printf("CURRENT ROUTE: %s\n", route.method);
+            }
+            else
+            {
+                printf("RDEFINED ROUTE; %s", route.path);
+                printf("Route path is NULL\n");
+            }
+        }
+        else
+        {
+            printf("Invalid route\n");
+        }
+    }
+
+    presentation(server->port);
+    while (1)
+    {
+        new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
+        read(new_socket, buffer, 30000);
+        printf("%s\n", buffer);
+        printf("%s\n", json);
+        // Extract HTTP method and URL path from the incoming request
+        char http_method[10];
+        char requested_url[256];
+        sscanf(buffer, "%9s %255s", http_method, requested_url);
+
+        if (route_handler(http_method, requested_url, &config, &new_socket) < 0)
+        {
+            if (response(new_socket, HTTP_STATUS_NOT_FOUND, "{"
+                                                            "   \"error\": \"not found\""
+                                                            "}") < 0)
+            {
+                close(new_socket);
+            }
+        }
+        close(new_socket);
+    }
 }
